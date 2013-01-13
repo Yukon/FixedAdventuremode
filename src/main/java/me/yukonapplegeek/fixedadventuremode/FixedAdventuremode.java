ebd13636371513ee6c870ebfe2c061705ce2027c
YukonAppleGeek <yukonvinecki@Yukon-Vineckis-MacBook-Pro.local>
2013-01-13 13:34:56 -0800

    Updated to exclue all block breaks and updated to 1.4.6

--- src/main/java/me/yukonapplegeek/fixedadventuremode/FixedAdventuremode.java
+++ src/main/java/me/yukonapplegeek/fixedadventuremode/FixedAdventuremode.java
@@ -1,9 +1,6 @@
 package me.yukonapplegeek.fixedadventuremode;
 
-import java.util.HashMap;
-
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,7 +11,6 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FixedAdventuremode extends JavaPlugin implements Listener {
-    private HashMap<Material, Boolean> blocks = new HashMap<Material, Boolean>();
 
     public void onDisable() {
         // TODO: Place any custom disable code here.
@@ -22,27 +18,12 @@ public class FixedAdventuremode extends JavaPlugin implements Listener {
 
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        blocks.put(Material.GLASS, true);
-        blocks.put(Material.THIN_GLASS, true);
-        blocks.put(Material.SKULL, true);
-        blocks.put(Material.TRIPWIRE, true);
-        blocks.put(Material.TRIPWIRE_HOOK, true);
-        blocks.put(Material.GLOWSTONE, true);
-        blocks.put(Material.REDSTONE_LAMP_ON, true);
-        blocks.put(Material.REDSTONE_LAMP_OFF, true);
-        blocks.put(Material.REDSTONE_WIRE, true);
-        blocks.put(Material.REDSTONE_TORCH_OFF, true);
-        blocks.put(Material.REDSTONE_TORCH_ON, true);
-        blocks.put(Material.STONE_BUTTON, true);
-        blocks.put(Material.WOOD_BUTTON, true);
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         if (event.getPlayer().getGameMode() == GameMode.ADVENTURE){
-            if (blocks.containsKey(event.getBlock().getType())){
-                event.setCancelled(true);
-            }
+            event.setCancelled(true);
         }
     }
 
